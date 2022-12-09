81205840a1b1bad65dbe376dd23c1b121477049e
LieseCordeyn <liese.cordeyn@student.ehb.be>
2022-12-09 15:04:55 +0100

    Added functionality to filter

--- src/Components/Works/WorkList.tsx
+++ src/Components/Works/WorkList.tsx
@@ -6,18 +6,28 @@ import Work from "./Work"
 
 function WorkList(){
     const [works, setWorks] = useState(Array<WorkType>)
+    const [chosenWorks, setChosenWorks] = useState(Array<WorkType>)
+    let [selectedFilter, setSelectedFilter] = useState(String)
+
 
     useEffect(() => {
+        setSelectedFilter("Featured")
         worksService.fetchWorks()
             .then(Response => {
                 setWorks(Response)
+                setChosenWorks(works.filter(x => x.Categories.includes("Featured")))
             })
     }, [])
 
+    const handleChange = (filter: any) => {
+        setSelectedFilter(filter)
+        setChosenWorks(works.filter(x => x.Categories.includes(filter)))
+    }
+
     return(
     <div id="WorkLists">
-        <Filter/>
-        {works.length > 0? works.map((x: WorkType) => {
+        <Filter onFilter={handleChange} filter={selectedFilter}/>
+        {works.length > 0? chosenWorks.map((x: WorkType) => {
             return(<Work key={x._id} _id={x._id} Title={x.Title} Categories={x.Categories} Description={x.Description} GithubLink={x.GithubLink} ImageUrls={x.ImageUrls} Tags={x.Tags}/>)
         })
         :
--- src/Components/Works/filter.tsx
+++ src/Components/Works/filter.tsx
@@ -1,23 +1,19 @@
 import { useEffect, useState } from "react"
 
-function Filter(){
-    let [selectedFilter, setSelectedFilter] = useState(String)
+function Filter(props: { onFilter: (arg0: any) => void; filter: string }){
 
     const options = ["Featured", "Final Works", "Expert Lab"]
 
-    useEffect(() => {
-        setSelectedFilter("Featured")
-    }, [])
 
     const changeFilter = (filter: any) => {
-            setSelectedFilter(filter)
+            props.onFilter(filter)
     }
 
 
     return (<div id="filter">
                 <div id="categories">
                     {options.map((x) => {
-                       return( <p className={selectedFilter === x ? 'activeClass' : ''} onClick={() => changeFilter(x)}>{x}</p>)
+                       return( <p className={props.filter === x ? 'activeClass' : ''} onClick={() => changeFilter(x)}>{x}</p>)
                     })}
                 </div>
                 <div id="years">
